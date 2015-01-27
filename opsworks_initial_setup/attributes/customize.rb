272d0659cce5025ea26b275666bac53b5057598c
Mark <iviarki@gmail.com>
2015-01-27 12:26:09 +0200

    Boib

---
+++ opsworks_initial_setup/attributes/customize.rb
@@ -0,0 +1,5 @@
+# Remove /var/www from mount
+default[:opsworks_initial_setup][:bind_mounts][:mounts] = {
+'/var/log/mysql' => "#{node[:opsworks_initial_setup][:ephemeral_mount_point]}/var/log/mysql",
+'/srv/www' => "#{node[:opsworks_initial_setup][:ephemeral_mount_point]}/srv/www"
+}
--- splunk/recipes/default.rb
+++
@@ -1,8 +0,0 @@
-remote_file "/tmp/splunk.rpm" do
-  source "https://www.splunk.com/page/download_track?file=6.2.1/splunk/linux/splunk-6.2.1-245427-linux-2.6-x86_64.rpm&platform=Linux&architecture=x86_64&version=6.2.1&product=splunk&typed=release&name=linux_installer&d=pro&wget=true"
-end
-
-package "splunk" do
-  action :install
-  source "/tmp/splunk.rpm"
-end
