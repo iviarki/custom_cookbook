886c0c4e7750de1a3b365360e483c819f08606d0
Mark <iviarki@gmail.com>
2015-04-23 10:47:58 +0200

    Bob

--- dnsupdate/recipes/add2.rb
+++ dnsupdate/recipes/add2.rb
@@ -5,7 +5,7 @@ ruby_block "add-record" do
       :access_key_id => "#{node[:dns_iam_key]}",
       :secret_access_key => "#{node[:dns_iam_secret]}")
     resp = route53.change_resource_record_sets(
-      #hosted_zone_id: "ResourceId",
+      #hosted_zone_id: ResourceId,
       hosted_zone_id: "Z3SMY8KPAK9IH5",
       change_batch: {
         changes: [
@@ -13,13 +13,12 @@ ruby_block "add-record" do
             action: "CREATE",
             resource_record_set: {
               #name: node[:opsworks][:instance][:hostname] + '.' + domain_name,
-              name: "vpn2.tomzynski.co.za",
+              name: node[:opsworks][:instance][:hostname] + '.tomzynski.co.za',
               type: "A",
               ttl: 60,
               resource_records: [
                 {
-                  #value: node[:opsworks][:instance][:private_ip],
-                  value: "192.168.21.12",
+                  value: node[:opsworks][:instance][:private_ip]
                 },
               ]
             }
