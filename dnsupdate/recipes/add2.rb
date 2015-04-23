6ef083216d4d246cf075b28088bee6bd8b31d343
Mark <iviarki@gmail.com>
2015-04-23 10:20:22 +0200

    bob

---
+++ dnsupdate/recipes/add2.rb
@@ -0,0 +1,23 @@
+resp = route53.change_resource_record_sets(
+  #hosted_zone_id: "ResourceId",
+  hosted_zone_id: "Z3SMY8KPAK9IH5",
+  change_batch: {
+    changes: [
+      {
+        action: "CREATE",
+        resource_record_set: {
+          #name: node[:opsworks][:instance][:hostname] + '.' + domain_name,
+          name: "vpn2.tomzynski.co.za",
+          type: "A",
+          ttl: 60,
+          resource_records: [
+            {
+              #value: node[:opsworks][:instance][:private_ip],
+              value: "192.168.21.12",
+            },
+          ]
+        }
+      }
+    ]
+  }
+)
