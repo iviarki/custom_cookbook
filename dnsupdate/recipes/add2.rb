24c205f07d42529a1bb2de018a55547c6f8f8c0b
Mark <iviarki@gmail.com>
2015-04-23 10:39:00 +0200

    bob

--- dnsupdate/recipes/add2.rb
+++ dnsupdate/recipes/add2.rb
@@ -1,25 +1,31 @@
-require 'aws-sdk'
-route53 = ::Aws::Route53::Client.new(region: 'us-east-1')
-resp = route53.change_resource_record_sets(
-  #hosted_zone_id: "ResourceId",
-  hosted_zone_id: "Z3SMY8KPAK9IH5",
-  change_batch: {
-    changes: [
-      {
-        action: "CREATE",
-        resource_record_set: {
-          #name: node[:opsworks][:instance][:hostname] + '.' + domain_name,
-          name: "vpn2.tomzynski.co.za",
-          type: "A",
-          ttl: 60,
-          resource_records: [
-            {
-              #value: node[:opsworks][:instance][:private_ip],
-              value: "192.168.21.12",
-            },
-          ]
-        }
+ruby_block "add-record" do
+  block do
+    require 'aws-sdk'
+    route53 = ::Aws::Route53::Client.new(region: 'us-east-1')
+    resp = route53.change_resource_record_sets(
+      #hosted_zone_id: "ResourceId",
+      hosted_zone_id: "Z3SMY8KPAK9IH5",
+      change_batch: {
+        changes: [
+          {
+            action: "CREATE",
+            resource_record_set: {
+              #name: node[:opsworks][:instance][:hostname] + '.' + domain_name,
+              name: "vpn2.tomzynski.co.za",
+              type: "A",
+              ttl: 60,
+              resource_records: [
+                {
+                  #value: node[:opsworks][:instance][:private_ip],
+                  value: "192.168.21.12",
+                },
+              ]
+            }
+          }
+        ]
       }
-    ]
-  }
-)
+    )
+  end
+  action :run
+end
+
