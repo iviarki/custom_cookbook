dfdf337f6de93cd2ed4480c77a456c2aea017010
Mark <iviarki@gmail.com>
2015-04-23 10:45:55 +0200

    Bob

--- dnsupdate/recipes/add2.rb
+++ dnsupdate/recipes/add2.rb
@@ -1,7 +1,9 @@
 ruby_block "add-record" do
   block do
     require 'aws-sdk'
-    route53 = ::AWS::Route53::Client.new(region: 'us-east-1')
+    route53 = ::AWS::Route53::Client.new(
+      :access_key_id => "#{node[:dns_iam_key]}",
+      :secret_access_key => "#{node[:dns_iam_secret]}")
     resp = route53.change_resource_record_sets(
       #hosted_zone_id: "ResourceId",
       hosted_zone_id: "Z3SMY8KPAK9IH5",
