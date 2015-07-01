eadbea9a0cfbb1284aafb9a7c92cf6b87195506a
Mark <iviarki@gmail.com>
2015-07-01 06:23:06 +0200

    New php_perms branch

---
+++ php_perms/recipes/default.rb
@@ -0,0 +1,13 @@
+Chef::Log.info("Running Permissions Recipe")
+
+execute "chown-data-www" do
+  command "chown -R www-data:www-data /var/www/myfoler"
+  user "root"
+  action :run
+end
+
+execute "chmod-data-www" do
+  command "chmod -R 777 /var/www/myfoler"
+  user "root"
+  action :run
+end
