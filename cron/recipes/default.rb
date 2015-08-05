e9631c0eb68ac860115d84bb8ccca1a984e7ea50
Mark <iviarki@gmail.com>
2015-08-05 07:44:37 +0200

    Modify for Ryan

--- cron/recipes/default.rb
+++ cron/recipes/default.rb
@@ -3,6 +3,6 @@ if node[:opsworks][:instance][:hostname] == 'php-app1'
     hour "1"
     minute "10"
     weekday "6"
-    command "cd /srv/www/myapp/current && php .lib/mailing.php"
+    command "php .lib/mailing.php"
   end
 end
