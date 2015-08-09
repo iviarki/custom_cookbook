8090be5ea2317650477aae2a07230d38a3b286f0
iviarki <iviarki@gmail.com>
2015-08-09 09:07:45 +0200

    Var log attempt

--- opsworks_initial_setup/attributes/customize.rb
+++ opsworks_initial_setup/attributes/customize.rb
@@ -1,6 +1,5 @@
-# Remove /var/www from mount
-#default[:opsworks_initial_setup][:bind_mounts][:mounts] = {
-#'/var/log/mysql' => "#{node[:opsworks_initial_setup][:ephemeral_mount_point]}/var/log/mysql",
-#'/srv/www' => "#{node[:opsworks_initial_setup][:ephemeral_mount_point]}/srv/www"
-#}
-default[:opsworks_initial_setup][:ephemeral_mount_point] = "/var/log"
+default[:opsworks_initial_setup][:bind_mounts][:mounts] = {
+  "/var/log" => "#{node[:opsworks_initial_setup][:ephemeral_mount_point]}/var/log",
+  '/srv/www' => "#{node[:opsworks_initial_setup][:ephemeral_mount_point]}/srv/www",
+  '/var/www' => "#{node[:opsworks_initial_setup][:ephemeral_mount_point]}/var/www",
+}
