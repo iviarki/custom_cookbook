93eb07cc31dc802e3bd176643006729877452287
iviarki <iviarki@gmail.com>
2015-08-09 08:06:23 +0200

    Use ephemeral as log folder

--- opsworks_initial_setup/attributes/customize.rb
+++ opsworks_initial_setup/attributes/customize.rb
@@ -1,5 +1,6 @@
 # Remove /var/www from mount
-default[:opsworks_initial_setup][:bind_mounts][:mounts] = {
-'/var/log/mysql' => "#{node[:opsworks_initial_setup][:ephemeral_mount_point]}/var/log/mysql",
-'/srv/www' => "#{node[:opsworks_initial_setup][:ephemeral_mount_point]}/srv/www"
-}
+#default[:opsworks_initial_setup][:bind_mounts][:mounts] = {
+#'/var/log/mysql' => "#{node[:opsworks_initial_setup][:ephemeral_mount_point]}/var/log/mysql",
+#'/srv/www' => "#{node[:opsworks_initial_setup][:ephemeral_mount_point]}/srv/www"
+#}
+default[:opsworks_initial_setup][:ephemeral_mount_point] = "/var/log"
