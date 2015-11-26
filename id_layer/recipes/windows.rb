893a15e273052f49653bf74cc1090e63777e44f0
Mark Tomzynski <tomzynsk@amazon.com>
2015-11-26 10:24:47 +0200

    Windows recipe

--- id_layer/recipes/windows.rb
+++ id_layer/recipes/windows.rb
@@ -1,4 +1,11 @@
-layer_name = search(:aws_opsworks_layer, "name")
+host_name = Socket.gethostname
+
+file 'C:\\hostname.txt' do
+  content "#{hostname}"
+end
+
+
+layer_name = search(:node)
 
 file 'C:\\something.txt' do
   content "#{layer_name}"
