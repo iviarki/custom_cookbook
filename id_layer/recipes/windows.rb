62a42c8adbc189dbd1f8a4da3012c28762818adf
Mark Tomzynski <tomzynsk@amazon.com>
2015-11-26 12:07:13 +0200

    Windows recipe

--- id_layer/recipes/windows.rb
+++ id_layer/recipes/windows.rb
@@ -1,12 +1,12 @@
 host_name = Socket.gethostname
 
 file 'C:\\hostname.txt' do
-  content "#{hostname}"
+  content "#{host_name}"
 end
 
 
 layer_name = search(:node)
 
 file 'C:\\something.txt' do
-  content "#{layer_name}"
+  content "#{layer_name[:name]}"
 end
