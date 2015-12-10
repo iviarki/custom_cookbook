84df58a5235db85c3e7ade1701a404913b6ea33f
iviarki <iviarki@gmail.com>
2015-12-10 10:59:38 +0200

    Update default.rb

--- krypton/recipes/default.rb
+++ krypton/recipes/default.rb
@@ -1,10 +1,16 @@
-template "/etc/newrelic/nrsysmond.cfg" do
-    source "nrsysmond.cfg.erb"
-    variables ({
-        :newrelic_key => node[:deploy]['krypton'][:environment_variables][:NEWRELIC_KEY],
-        :hostname => "#{node[:opsworks][:instance][:region]}-#{node[:opsworks][:instance][:hostname]}"
-    })
-   user "root"
-   group "newrelic"
-   mode "0644"
+node[:command][:args][:app_ids].each | appid
+  node[:resources][:apps].each | app
+    if app[:app_id]?(appid)
+        template "/tmp/nrsysmond.cfg" do
+            source "temp.cfg.erb"
+            variables ({
+             :newrelic_key => node[:app][:environment][:newrelic_key]
+            })
+            user "root"
+            group "root"
+            mode "0644"
+        end
+    end
+ end
 end
+ 
