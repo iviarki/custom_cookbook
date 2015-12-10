e1bc274b420c0daac00e7efe12880addf78f0e43
iviarki <iviarki@gmail.com>
2015-12-10 11:19:30 +0200

    Update default.rb

--- krypton/recipes/default.rb
+++ krypton/recipes/default.rb
@@ -1,16 +1,2 @@
-node[:aws_opsworks_agent][:command][:args][:app_ids].each do |appid|
-  node[:aws_opsworks_agent][:resources][:apps].each do |app|
-    if app[:app_id] == appid
-        template "/tmp/nrsysmond.cfg" do
-            source "test.cfg.erb"
-            variables ({
-             :newrelic_key => node[:app][:environment][:newrelic_key]
-            })
-            user "root"
-            group "root"
-            mode "0644"
-        end
-    end
- end
-end
- 
+app = search(:aws_opsworks_app, "name:krypton").first
+Chef::Log.info(”Appenv: #{app[:environment][:newrelic_key]}")
