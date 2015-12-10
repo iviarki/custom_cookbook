92280529c2860d6f7e85cb99ec3a9db2782157c3
iviarki <iviarki@gmail.com>
2015-12-10 11:25:59 +0200

    Update default.rb

--- krypton/recipes/default.rb
+++ krypton/recipes/default.rb
@@ -1,2 +1,13 @@
 app = search(:aws_opsworks_app, "name:krypton").first
-Chef::Log.info("Appenv: #{app[:environment][:newrelic_key]}")
+app[:environment][:newrelic_key]
+
+
+template "/etc/newrelic/nrsysmond.cfg" do
+  source "nrsysmond.cfg.erb"
+  variables ({
+    :newrelic_key => app[:environment][:newrelic_key],
+  })
+  user "root"
+  group "newrelic"
+  mode "0644"
+end
