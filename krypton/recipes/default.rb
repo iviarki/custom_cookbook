30136fbb10c1b58e73e6d49885a7ae419382b1c0
iviarki <iviarki@gmail.com>
2015-12-10 11:00:10 +0200

    Update default.rb

--- krypton/recipes/default.rb
+++ krypton/recipes/default.rb
@@ -2,7 +2,7 @@ node[:command][:args][:app_ids].each | appid
   node[:resources][:apps].each | app
     if app[:app_id]?(appid)
         template "/tmp/nrsysmond.cfg" do
-            source "temp.cfg.erb"
+            source "test.cfg.erb"
             variables ({
              :newrelic_key => node[:app][:environment][:newrelic_key]
             })
