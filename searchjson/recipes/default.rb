52aa34595b9f4283ab38ddeb9e305f6e84dbd49b
Tomzynski <tomzynsk@acbc32a51b9f.ant.amazon.com>
2016-02-10 11:21:07 +0200

    added)

--- searchjson/recipes/default.rb
+++ searchjson/recipes/default.rb
@@ -1,5 +1,5 @@
 node['apps'].each do |app|
-  resultapp = search(:aws_opsworks_app, "name:#{app}".first)
+  resultapp = search(:aws_opsworks_app, "name:#{app}").first
   file "/tmp/#{resultapp[:name]}" do
     content "#{resultapp[:name]}"
     mode 0644
