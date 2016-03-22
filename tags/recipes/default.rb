6cd32b15c5dc2ad0b8cd3f48d64b5a7d6eec697e
Tomzynski <tomzynsk@amazon.com>
2016-03-22 13:21:06 +0200

    New Commit

--- tags/recipes/default.rb
+++ tags/recipes/default.rb
@@ -6,10 +6,10 @@ gem 'aws-sdk'
 require 'aws-sdk'
 instance = search("aws_opsworks_instance").first
 toTag = Array.new
-toTag.push(instance['instance_id'])
+toTag.push(instance['ec2_instance_id'])
 ec2 = Aws::EC2::Client.new( :region => "us-east-1")
 instance_attributes = ec2.describe_instance_attribute({
-  instance_id: instance['instance_id'],
+  instance_id: instance['ec2_instance_id'],
   attribute: "blockDeviceMapping"
 })
 instance_attributes.block_device_mappings.each do|mapping|
