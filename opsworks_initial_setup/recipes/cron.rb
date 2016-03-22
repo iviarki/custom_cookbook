60a15bf12521fa892c37140810204cbbf20e4d23
Tomzynski <tomzynsk@amazon.com>
2016-03-22 12:58:10 +0200

    New Commit

--- opsworks_initial_setup/recipes/cron.rb
+++
@@ -1,6 +0,0 @@
-cron "job_name" do
-  hour "1"
-  minute "10"
-  weekday "6"
-  command "php .lib/mailing.php"
-end
---
+++ tags/recipes/default.rb
@@ -0,0 +1,17 @@
+gem 'aws-sdk'
+require 'aws-sdk'
+instance = search("aws_opsworks_instance").first
+toTag = Array.new
+toTag.push(instance['instance_id'])
+ec2 = Aws::EC2::Client.new( :region => "us-east-1")
+instance_attributes = ec2.describe_instance_attribute({
+  instance_id: instance['instance_id'],
+  attribute: "blockDeviceMapping"
+})
+instance_attributes.block_device_mappings.each do|mapping|
+  toTag.push(mapping.ebs.volume_id)
+end
+tag_instance = ec2.create_tags({
+  resources: toTag,
+  tags: node['state']
+})
