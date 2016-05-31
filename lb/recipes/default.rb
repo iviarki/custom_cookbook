49b2894d9d6836313c95fbe8e50b630a54c83e2e
Tomzynski <tomzynsk@amazon.com>
2016-05-31 09:40:24 +0200

    Load Balance

--- lb/recipes/default.rb
+++ lb/recipes/default.rb
@@ -6,7 +6,7 @@ gem 'aws-sdk'
 require 'aws-sdk'
 instance = search("aws_opsworks_instance", "self:true").first
 
-if instance['hostname'].exclude? "worker" do
+if instance['hostname'].exclude? "worker"
   elasticloadbalancingClient = Aws::ElasticLoadBalancing::Client.new()
   attach = elasticloadbalancingClient.register_instances_with_load_balancer({
     load_balancer_name: node['rails_elb_name'],
