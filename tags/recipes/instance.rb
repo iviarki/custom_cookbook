14e4522bbb65ae41cc80f257d2e342409a77530c
Tomzynski <tomzynsk@amazon.com>
2016-06-13 20:21:44 +0200

    Passed tags as array

--- tags/recipes/instance.rb
+++ tags/recipes/instance.rb
@@ -3,22 +3,10 @@ chef_gem "aws-sdk" do
 end
 gem 'aws-sdk'
 require 'aws-sdk'
-
-stack = search("aws_opsworks_stack").first
-tag = stack['name']+ '-'
-
 instance = search("aws_opsworks_instance", "self:true").first
-tag += instance['hostname']
-
-search("aws_opsworks_layer").each do |layer|
-  if instance['layer_ids'].include? layer['layer_id']
-    tag += layer['name']+ '-'
-  end
-end
-
 ec2 = Aws::EC2::Client.new( :region => "eu-west-1" )
 ec2.create_tags({
   resources: [instance['ec2_instance_id']],
-  tags: [{ key: "OpsWorksTag",
-    value: tag }]
+  tags: { key: "OWLayer",
+    value: "OpsWorksRG1" }
 })
