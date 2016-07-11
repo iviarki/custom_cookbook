f810aef735351d820816762ed227fe5f78b44be4
Tomzynski <tomzynsk@amazon.com>
2016-07-11 07:02:49 +0200

    Create user and folder as required

--- createFile/recipes/default.rb
+++ createFile/recipes/default.rb
@@ -1,4 +1,18 @@
-file "/srv/.ssh/id_rsa" do
+user_home = "/home/#{node['user']}"
+
+user node['user'] do
+   shell '/bin/bash'
+   home user_home
+   system true
+   action :create
+end
+
+directory "#{user_home}/.ssh" do
+   owner node[:user][:username]
+   recursive true
+end
+
+file "#{user_home}/.ssh/id_rsa" do
    content "Sample Text"
-   mode 0600
+   mode 0400
 end
