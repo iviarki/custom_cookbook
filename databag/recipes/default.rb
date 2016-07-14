b881f818c9218920df628fe4098ce46ed0346402
Tomzynski <tomzynsk@amazon.com>
2016-07-14 14:05:34 +0200

    hehe

--- databag/recipes/default.rb
+++ databag/recipes/default.rb
@@ -1,6 +1,11 @@
 instance = search("aws_opsworks_instance", "self:true").first
 hostname = instance['hostname']
-Chef::Log.info("Hostname: #{hostname}")
+Chef::Log.info("Hostname: #{"hostname")
+layer_ids = instance['layer_ids']
+layer_ids.each do |layer|
+	Chef::Log.info("Layer ID: #{layer)
+	Chef::Log.info("Layer Name: #{search("aws_opsworks_layer", "layer_id:" + layer).first['name']}")
+end
 stack = search(:aws_opsworks_stack).first
 ec2_region = stack['region']
 Chef::Log.info("Region: #{ec2_region}")
