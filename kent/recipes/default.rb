983743ed5be95afd5cca168c58b1e1c04079b227
Tomzynski <tomzynsk@amazon.com>
2016-11-09 12:39:48 +0200

    New test

--- kent/recipes/default.rb
+++
@@ -1,4 +0,0 @@
-node['opsworks']['instance']['layers'].each do | layer |
-  link = node[layer]["S3Link"]
-  Chef::Log.info("********** The app's initial background color is '#{link}' **************")
-end
--- searchjson/recipes/default.rb
+++
@@ -1,8 +0,0 @@
-node['apps'].each do |app|
-  resultapp = search(:aws_opsworks_app, "name:#{app}").first
-  file "/tmp/#{resultapp[:name]}" do
-    content "#{resultapp[:name]}"
-    mode 0644
-    action :create
-  end
-end
