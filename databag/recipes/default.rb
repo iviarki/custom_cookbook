4b941615f2d3797d25668dfdc5fa8e59221c3942
Mark Tomzynski <tomzynsk@amazon.com>
2017-06-04 08:44:39 +0200

    IAM user added

--- databag/recipes/default.rb
+++ databag/recipes/default.rb
@@ -1,3 +1,4 @@
 command = search("aws_opsworks_command").first
 Chef::Log.info("********** The command's type is '#{command['type']}' **********")
 Chef::Log.info("********** The command was sent at '#{command['sent_at']}' **********")
+Chef::Log.info("********** The command was sent at '#{command['iam_user_arn']}' **********")
