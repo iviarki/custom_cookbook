7016bed7f799a8d0c9fa6e1fd668498fb76e50af
Mark Tomzynski <tomzynsk@amazon.com>
2017-06-04 10:10:52 +0200

    Bob

--- databag/recipes/default.rb
+++ databag/recipes/default.rb
@@ -1,4 +1,25 @@
 command = search("aws_opsworks_command").first
-Chef::Log.info("********** The command's type is '#{command['type']}' **********")
-Chef::Log.info("********** The command was sent at '#{command['sent_at']}' **********")
-Chef::Log.info("********** The command was sent at '#{command['iam_user_arn']}' **********")
+
+Chef::Log.info("********** Command issued by: '#{command['iam_user_arn'].include?("arn") ? command['iam_user_arn'] : get_iam_user(command['command_id'])}' **********")
+
+def get_iam_user(command_id)
+  # Ensure aws-sdk is installed and create OpsWorks Client
+  chef_gem "aws-sdk" do
+    action :install
+    compile_time true
+  end
+  require 'aws-sdk'
+  client = Aws::OpsWorks::Client.new()
+
+  # Get deployment ID
+  command_resp = client.describe_commands({
+    command_ids: [command_id]
+  })
+  deployment_id = command_resp.commands[0].deployment_id
+
+  # Get IAM User from Deployment ID
+  deployment_resp = client.describe_deployments({
+    deployment_ids: [deployment_id],
+  })
+  return deployment_resp.deployments[0].iam_user_arn
+end
