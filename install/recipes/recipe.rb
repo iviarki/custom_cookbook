77f5fa774f34455bf8aac86e764c6f079a09578d
Mark Tomzynski <tomzynsk@amazon.com>
2019-06-02 14:01:07 +0200

    2

--- install/recipes/recipe.rb
+++ install/recipes/recipe.rb
@@ -1,5 +1,5 @@
 powershell_script 'p4vinst64_install' do 
 	cwd "C:\\"
-	code '.\p4vinst64.exe /s /v/qn' 
+	code '.\p4vinst64.exe /s /v/qn >> C:\\log.log' 
 	flags "-NoLogo, -NonInteractive, -NoProfile, -ExecutionPolicy Unrestricted, -InputFormat None, -File"
 end 
