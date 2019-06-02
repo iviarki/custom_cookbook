c95dc3ca00df4719465d43ff434b403f3ea54b40
Mark Tomzynski <tomzynsk@amazon.com>
2019-06-02 11:38:06 +0200

    2

--- install/recipes/recipe.rb
+++ install/recipes/recipe.rb
@@ -1,5 +1,6 @@
-windows_package 'Microsoft Visual C++ 2005 Redistributable' do
-  source 'http://www.perforce.com/downloads/perforce/r19.1/bin.ntx64/p4vinst64.exe'
-  installer_type :custom
-  options '/s /v/qn'
-end
+execute 'p4vinst64_download' do 
+	command 'powershell.exe Invoke-WebRequest -Uri http://www.perforce.com/downloads/perforce/r19.1/bin.ntx64/p4vinst64.exe -OutFile C:\temp\p4vinst64.exe' 
+end 
+execute 'p4vinst64_install' do 
+	command 'C:\temp\p4vinst64.exe /s /v/qn' 
+end 
