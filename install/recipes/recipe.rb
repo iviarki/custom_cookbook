d0406099cb3400f05385e1d20367bc81c673e3d5
Mark Tomzynski <tomzynsk@amazon.com>
2019-06-02 11:52:17 +0200

    2

--- install/recipes/recipe.rb
+++ install/recipes/recipe.rb
@@ -1,8 +1,8 @@
 execute 'p4vinst64_download' do
-	cwd "C:\\temp"
+	cwd "C:\\"
 	command 'powershell.exe Invoke-WebRequest -Uri http://www.perforce.com/downloads/perforce/r19.1/bin.ntx64/p4vinst64.exe -OutFile p4vinst64.exe' 
 end 
 execute 'p4vinst64_install' do 
-	cwd "C:\\temp"
+	cwd "C:\\"
 	command 'p4vinst64.exe /s /v/qn' 
 end 
