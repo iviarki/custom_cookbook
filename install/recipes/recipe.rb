0eafc9aa8716266b79b4163ffaae4e9b6d5a16fe
Mark Tomzynski <tomzynsk@amazon.com>
2019-06-03 09:53:18 +0200

    3

--- install/recipes/recipe.rb
+++ install/recipes/recipe.rb
@@ -1,4 +1,4 @@
 powershell_script 'p4vinst64_install' do 
 	cwd "C:\\"
-	code `Start-Process -FilePath "C:\p4vinst64.exe" -ArgumentList '/s /v"/l*v \"p4v.log\""/qn' -LoadUserProfile -NoNewWindow -PassThru -RedirectStandardError error.txt -RedirectStandardOutput std.txt -Wait -Debug -Verbose`
+	code "Start-Process -FilePath "C:\p4vinst64.exe" -ArgumentList '/s /v\"/l*v \"p4v.log\"\"/qn' -LoadUserProfile -NoNewWindow -PassThru -RedirectStandardError error.txt -RedirectStandardOutput std.txt -Wait -Debug -Verbose"
 end 
