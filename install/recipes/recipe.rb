e42de66270fd11d12ef066404f7b6c443e43e140
Mark Tomzynski <tomzynsk@amazon.com>
2019-06-03 11:23:58 +0200

    4

--- install/recipes/recipe.rb
+++ install/recipes/recipe.rb
@@ -1,5 +1,5 @@
 powershell_script 'p4vinst64_install' do 
 	cwd "C:\\"
-	code 'Start-Process -FilePath C:\p4vinst64.exe -ArgumentList "/s /v/qn" -Wait -Debug -Verbose'
+	code 'Start-Process -FilePath C:\p4vinst64.exe -ArgumentList "/s /v/qn" -Wait -Debug -Verbose -PassThru -Verb RunAs -LoadUserProfile'
         flags '-ExecutionPolicy Bypass'
 end 
