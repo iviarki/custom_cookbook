abeb9a9d76290bd34cf28d1358fa3eedd8d22d73
Mark Tomzynski <tomzynsk@amazon.com>
2019-06-03 08:00:03 +0200

    3

--- install/recipes/recipe.rb
+++ install/recipes/recipe.rb
@@ -1,4 +1,4 @@
 powershell_script 'p4vinst64_install' do 
 	cwd "C:\\"
-	code 'start-process -FilePath "C:\p4vinst64.exe" -ArgumentList "/s /v/qn" -Verb runas -Wait'
+	code 'start-process -FilePath "C:\p4vinst64.exe" -ArgumentList "/s /v/qn" -Verb runas -Wait -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt'
 end 
