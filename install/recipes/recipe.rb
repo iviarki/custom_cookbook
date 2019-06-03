37ece41d828340b415f54389b347ad7ac5ad812a
Mark Tomzynski <tomzynsk@amazon.com>
2019-06-03 12:31:12 +0200

    Added Windows Cookbook

--- install/recipes/recipe.rb
+++ install/recipes/recipe.rb
@@ -2,5 +2,5 @@ windows_package 'PuTTY version 0.60' do
   source 'http://www.perforce.com/downloads/perforce/r19.1/bin.ntx64/p4vinst64.exe'
   installer_type :custom
   action :install
-  options '/s /v/qn'
+  options '/v"/l*v+ c:\p4vinst.log"'
 end
