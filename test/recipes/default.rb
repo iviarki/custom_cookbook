ac01f6ce308aa6ec7b24b35b6044b7727c5deeea
Mark Tomzynski <tomzynsk@amazon.com>
2019-08-28 09:41:30 +0200

    Adjustment

--- test/recipes/default.rb
+++ test/recipes/default.rb
@@ -1,21 +1,24 @@
 layer = search("aws_opsworks_layer", "name:nginx").first
 Chef::Log.info("********** Layer: '#{layer}' **********")
-instances = search("aws_opsworks_instance", "layer_ids:'#{layer['layer_id']}'")
+instances = search("aws_opsworks_instance")
 Chef::Log.info("********** Instances: '#{instances}' **********")
 
-
+nginx_instances = instances.select do |i|
+  i.layer_ids.include? layer.layer_id
+end
+Chef::Log.info("********** Nginx Instances: '#{nginx_instances}' **********")
 execute 'output' do
   command 'echo "Output command has run"'
   action :nothing
 end
 
-template '/etc/motd' do
+template '/etc/bob' do
   source 'motd.erb'
   owner 'root'
   group 'root'
   mode '0755'
   variables(
-    :instances => instances
+    :instances => nginx_instances
   )
   notifies :run, 'execute[output]'
 end
